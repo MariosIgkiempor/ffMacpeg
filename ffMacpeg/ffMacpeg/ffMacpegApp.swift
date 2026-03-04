ee8f53f0a916ddb3e0d0348bf332665b51c98fa4
Mario Igkiempor <mario.igkiempor@theodo.com>
2026-03-04 19:11:30 +0000

    Init project w/ ffmpeg binary

---
+++ ffMacpeg/ffMacpeg/ffMacpegApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffMacpegApp.swift
+//  ffMacpeg
+//
+//  Created by Marios Igkiempor on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ffMacpegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
